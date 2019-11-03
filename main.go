295f4cf5a6560f5bfe6152d4db824b410e83aa23
Wallace Carr <commandercarr1@gmail.com>
2019-11-03 05:34:42 -0700

    update amount month calculation

--- main.go
+++ main.go
@@ -14,6 +14,23 @@ var daysTilEquilibrium int
 var daysTilNominal int
 
 type AmountCalculationMatrix struct {
+	ListAmountAtMonthInterval   []Month
+	AmountOfMonthsInCalculation int
+	StartingAmount              float64
+	FinalAmount                 float64
+}
+type Month struct {
+	AmountTotal  float64
+	AmountGained float64
+}
+
+type EquilibriumModel struct {
+	ListAmountAtMonthInterval  []float64
+	EquilibriumValue           float64
+	AmountNeededForEquilibrium float64
+}
+
+type NominalModel struct {
 	ListAmountAtMonthInterval   []float64
 	AmountOfMonthsInCalculation int
 	FinalAmount                 float64
@@ -30,31 +47,62 @@ func main() {
 		fmt.Println(v)
 		i++
 	}
+
+	calculateEquilibriumModel(amountCalculationMatrix)
+
+	// calculateNominalModel()
 	fmt.Scanln()
 }
+
+func calculateEquilibriumModel(amountCalculationMatrix AmountCalculationMatrix) EquilibriumModel {
+
+	//Given all metrics, at which day will equilibrium be met if any
+	equilibriumModel := EquilibriumModel{}
+
+	// costOfLiving
+	return equilibriumModel
+}
+
+// func calculateMonthGross(amountCalculationMatrix AmountCalculationMatrix){
+// 	for
+// }
+
+// func calculateNominal() {
+// }
 
 func compoundCalculation(startingValue float64, rate float64, amountGainPeriods int, amountmonths int) AmountCalculationMatrix {
 	updatedValue := 0.0
 	indexAmountMonths := 0
 	indexAmountGainPeriods := 0
-
 	amountCalculationMatrix := AmountCalculationMatrix{}
 	for indexAmountMonths < amountmonths {
+		month := Month{}
 		indexAmountGainPeriods = 0
 		if indexAmountMonths == 0 {
 			updatedValue = startingValue
-			amountCalculationMatrix.ListAmountAtMonthInterval = append(amountCalculationMatrix.ListAmountAtMonthInterval, startingValue)
+			amountCalculationMatrix.StartingAmount = startingValue
 			amountCalculationMatrix.AmountOfMonthsInCalculation = amountmonths
 		}
 		for indexAmountGainPeriods < amountGainPeriods {
 			updatedValue = updatedValue + (updatedValue * rate)
 			indexAmountGainPeriods++
 		}
-		amountCalculationMatrix.ListAmountAtMonthInterval = append(amountCalculationMatrix.ListAmountAtMonthInterval, updatedValue)
+		month.AmountTotal = updatedValue
+		amountCalculationMatrix.ListAmountAtMonthInterval = append(amountCalculationMatrix.ListAmountAtMonthInterval, month)
 		indexAmountMonths++
 		if indexAmountMonths == amountmonths {
 			amountCalculationMatrix.FinalAmount = updatedValue
 		}
+	}
+	//calculate amount of difference per month
+	previousMonthAmount := 0.0
+	for i, v := range amountCalculationMatrix.ListAmountAtMonthInterval {
+		if i == 0 {
+			previousMonthAmount = amountCalculationMatrix.StartingAmount
+		}
+		amountDifference := v.AmountTotal - previousMonthAmount
+		amountCalculationMatrix.ListAmountAtMonthInterval[i].AmountGained = amountDifference
+		previousMonthAmount = v.AmountTotal
 	}
 	return amountCalculationMatrix
 }
